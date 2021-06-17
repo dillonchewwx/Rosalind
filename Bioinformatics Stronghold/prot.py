31344345bdf329513cca73e1c3a55a6dd94ff290
dillonchewwx <dillonchewwx@gmail.com>
2021-06-18 02:41:35 +0800

    18/6/21 Update

---
+++ Bioinformatics Stronghold/prot.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 15 23:43:57 2021
+@author: dillonchewwx
+Solution to prot on rosalind.info
+"""
+
+def main():
+    file = open("rosalind_prot.txt")
+    RNAstring = file.read()
+    file.close()
+    print(translation(RNAstring))
+    
+def translation(RNAstring):
+    # load RNA codon table
+    RNA_to_AA = {"UUU" : "F", "CUU" : "L", "AUU" : "I", "GUU" : "V",
+           "UUC" : "F", "CUC" : "L", "AUC" : "I", "GUC" : "V",
+           "UUA" : "L", "CUA" : "L", "AUA" : "I", "GUA" : "V",
+           "UUG" : "L", "CUG" : "L", "AUG" : "M", "GUG" : "V",
+           "UCU" : "S", "CCU" : "P", "ACU" : "T", "GCU" : "A",
+           "UCC" : "S", "CCC" : "P", "ACC" : "T", "GCC" : "A",
+           "UCA" : "S", "CCA" : "P", "ACA" : "T", "GCA" : "A",
+           "UCG" : "S", "CCG" : "P", "ACG" : "T", "GCG" : "A",
+           "UAU" : "Y", "CAU" : "H", "AAU" : "N", "GAU" : "D",
+           "UAC" : "Y", "CAC" : "H", "AAC" : "N", "GAC" : "D",
+           "UAA" : "STOP", "CAA" : "Q", "AAA" : "K", "GAA" : "E",
+           "UAG" : "STOP", "CAG" : "Q", "AAG" : "K", "GAG" : "E",
+           "UGU" : "C", "CGU" : "R", "AGU" : "S", "GGU" : "G",
+           "UGC" : "C", "CGC" : "R", "AGC" : "S", "GGC" : "G",
+           "UGA" : "STOP", "CGA" : "R", "AGA" : "R", "GGA" : "G",
+           "UGG" : "W", "CGG" : "R", "AGG" : "R", "GGG" : "G" 
+           }
+    AA_seq=""
+    for i in range(0, len(RNAstring)-(3+len(RNAstring)%3), 3):
+        if RNA_to_AA[RNAstring[i:i+3]] == "STOP":
+            break
+        else:
+            AA_seq += RNA_to_AA[RNAstring[i:i+3]]
+    return AA_seq
+
+if __name__ == "__main__":
+    main()
